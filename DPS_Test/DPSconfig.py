ad2e8591cf4a5406896fc80370b1442af80df7f0
tomzono <68574400+tomzono@users.noreply.github.com>
2020-10-30 18:01:32 +0900

    DPS設定用のプログラム追記 configの接続文字列退避済み

---
+++ DPS_Test/DPSconfig.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import os
+
+#DPSConectionSetting
+provisioning_host = "PROVISIONING_HOST"
+id_scope = "PROVISIONING_IDSCOPE"
+registration_id = "PROVISIONING_REGISTRATION_ID"
+symmetric_key = os.getenv"PROVISIONING_SYMMETRIC_KEY"
