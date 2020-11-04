7c918c179ebeda50454a014a231a32ea3c56e305
tomzono <68574400+tomzono@users.noreply.github.com>
2020-11-04 11:29:45 +0900

    DPS登録グループ用のテストプログラム追加

--- DPS_Test/DPSconfig.py
+++ DPS_Test/DPSconfig.py
@@ -6,3 +6,6 @@ provisioning_host = "PROVISIONING_HOST"
 id_scope = "PROVISIONING_IDSCOPE"
 registration_id = "PROVISIONING_REGISTRATION_ID"
 symmetric_key = "PROVISIONING_SYMMETRIC_KEY"
+#DPSGroupSetting
+provisioning_groupe_host = "PROVISIONING_HOST"
+groupe_id_scope = "PROVISIONING_IDSCOPE"
