77e1f161d86aaa9fc1706a12ddfd85d85ddd014d
tomzono <68574400+tomzono@users.noreply.github.com>
2020-11-09 13:59:13 +0900

    ？？？

--- DPS_Test/DPS_GroupConection.py
+++ DPS_Test/DPS_GroupConection.py
@@ -30,13 +30,15 @@ device_ids_to_keys = {}
 results = {}
 
 derived_device_key = devicekey.derive_device_key(device_id, group_symmetric_key)
+device_ids_to_keys[device_id] = derived_device_key
 
 def register_device(registration_id):
+
     provisioning_device_client = ProvisioningDeviceClient.create_from_symmetric_key(
         provisioning_host=provisioning_host,
         registration_id=registration_id,
         id_scope=id_scope,
-        symmetric_key=derived_device_key,
+        symmetric_key=device_ids_to_keys[registration_id],
     )
     return provisioning_device_client.register()
 
