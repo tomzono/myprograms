cd94995ba8fd249cb93af2fdef0028db4d91f8fe
tomzono <68574400+tomzono@users.noreply.github.com>
2020-11-09 14:15:42 +0900

    コンパクトに修正

--- DPS_Test/DPS_GroupConection.py
+++ DPS_Test/DPS_GroupConection.py
@@ -25,56 +25,50 @@ group_symmetric_key = DPSconfig.group_symmetric_key
 # Please make sure that there are no spaces in these device ids.
 device_id = socket.gethostname()
 # For computation of device keys
-device_ids_to_keys = {}
 # Keep a dictionary for results
 results = {}
 
 derived_device_key = devicekey.derive_device_key(device_id, group_symmetric_key)
-device_ids_to_keys[device_id] = derived_device_key
 
 def register_device(registration_id):
-
     provisioning_device_client = ProvisioningDeviceClient.create_from_symmetric_key(
         provisioning_host=provisioning_host,
         registration_id=registration_id,
         id_scope=id_scope,
-        symmetric_key=device_ids_to_keys[registration_id],
+        symmetric_key=derived_device_key,
     )
     return provisioning_device_client.register()
 
-for device_id in device_ids_to_keys:
-    registration_result =register_device(registration_id=device_id)
-    results[device_id] = registration_result
+registration_result =register_device(registration_id=device_id)
 
-for device_id in device_ids_to_keys:
-    # The result can be directly printed to view the important details.
-    registration_result = results[device_id]
-    print(registration_result)
-    # Individual attributes can be seen as well
-    print("The status was :-")
-    print(registration_result.status)
-    print("The etag is :-")
-    print(registration_result.registration_state.etag)
-    print("\n")
-    if registration_result.status == "assigned":
-        print("Will send telemetry from the provisioned device with id {id}".format(id=device_id))
-        device_client = IoTHubDeviceClient.create_from_symmetric_key(
-            symmetric_key=device_ids_to_keys[device_id],
-            hostname=registration_result.registration_state.assigned_hub,
-            device_id=registration_result.registration_state.device_id,
-        )
 
-        # Connect the client.
-        device_client.connect()
 
-        # Send 5 messages
-        for i in range(1, 6):
-            print("sending message #" + str(i))
-            device_client.send_message("test payload message " + str(i))
-            time.sleep(0.5)
 
-        # finally, disconnect
-        device_client.disconnect()
+print(registration_result)
+print("The status was :-")
+print(registration_result.status)
+print("The etag is :-")
+print(registration_result.registration_state.etag)
+print("\n")
+if registration_result.status == "assigned":
+    print("Will send telemetry from the provisioned device with id {id}".format(id=device_id))
+    device_client = IoTHubDeviceClient.create_from_symmetric_key(
+        symmetric_key=derived_device_key,
+        hostname=registration_result.registration_state.assigned_hub,
+        device_id=registration_result.registration_state.device_id,
+    )
 
-    else:
-        print("Can not send telemetry from the provisioned device with id {id}".format(id=device_id))
+    # Connect the client.
+    device_client.connect()
+
+    # Send 5 messages
+    for i in range(1, 6):
+        print("sending message #" + str(i))
+        device_client.send_message("test payload message " + str(i))
+        time.sleep(0.5)
+
+    # finally, disconnect
+    device_client.disconnect()
+
+else:
+    print("Can not send telemetry from the provisioned device with id {id}".format(id=device_id))
